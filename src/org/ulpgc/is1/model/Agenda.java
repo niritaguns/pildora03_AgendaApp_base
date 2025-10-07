a16ca4dee7b0254e26ca0cd28d0e41445f233100
armas <armassniraa@gmail.com>
2025-10-07 15:31:57 +0100

    clase 30-09-2025/07-10-2025

--- src/org/ulpgc/is1/model/Agenda.java
+++ src/org/ulpgc/is1/model/Agenda.java
@@ -1,5 +1,52 @@
 package org.ulpgc.is1.model;
 
-public class Agenda {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Agenda {
+    private List<Contact> contacts;
+    private List<Group> groups;
+    private List<Member> members;
+
+    public Agenda() {
+        this.contacts = new ArrayList<>();
+        this.groups = new ArrayList<>();
+        this.members = new ArrayList<>();
+    }
+
+    public void addPerson(Person p) {
+        contacts.add(p);
+    }
+
+    public void addCompany(Company c) {
+        contacts.add(c);
+    }
+
+    public void addGroup(Group g) {
+        groups.add(g);
+    }
+
+    public List<Contact> getContactList() {
+        return contacts;
+    }
+
+    public List<Group> getGroupList() {
+        return groups;
+    }
+
+    public void addMember2Group(Contact c, Group g) {
+        g.addContact(c);  // añadimos el contacto al grupo
+        Member m = new Member(c, g);
+        members.add(m);
+        System.out.println("Contacto añadido al grupo el " + m.getFormattedDate());
+    }
+
+    public Member getMember(Contact c, Group g) {
+        for (Member m : members) {
+            if (m.getContact().equals(c) && m.getGroup().equals(g)) {
+                return m;
+            }
+        }
+        return null;
+    }
 }
--- src/org/ulpgc/is1/model/Member.java
+++ src/org/ulpgc/is1/model/Member.java
@@ -1,5 +1,34 @@
 package org.ulpgc.is1.model;
 
-public class Member {
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
+public class Member {
+    private Contact contact;
+    private Group group;
+    private LocalDateTime date;
+
+    public Member(Contact contact, Group group) {
+        this.contact = contact;
+        this.group = group;
+        this.date = LocalDateTime.now(); // guarda la fecha actual al crearse
+    }
+
+    public Contact getContact() {
+        return contact;
+    }
+
+    public Group getGroup() {
+        return group;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public String getFormattedDate() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        return date.format(formatter);
+    }
 }
+
